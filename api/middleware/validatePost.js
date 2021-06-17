dea700967de28a8d073e314242a7f3d2a25bd6ea
Ian Walter <pub@ianwalter.dev>
2021-06-16 22:05:56 -0400

    Adding validator middleware

--- api/middleware/validatePost.js
+++ api/middleware/validatePost.js
@@ -1,4 +1,37 @@
-export default function validatePost (ctx, next) {
-  // TODO:
-  return next()
+import { SchemaValidator, isString, isUrl } from '@ianwalter/nrg-validation'
+
+const canBeEmptyIfReply = {
+  validate (input, state, ctx) {
+    if (ctx.input.threadId && ctx.input.parentId) return { isValid: true }
+    return { isValid: input?.length }
+  }
+}
+const canBeEmptyIfPost = {
+  validate (input, state, ctx) {
+    if (ctx.input.title && ctx.input.link) return { isValid: true }
+    return { isValid: input?.length }
+  }
+}
+const postValidator = new SchemaValidator({
+  title: { isString, canBeEmptyIfReply },
+  link: { isString, isUrl, canBeEmptyIfReply },
+  body: { isString, canBeEmptyIfPost },
+  threadId: { isString, canBeEmptyIfPost },
+  parentId: { isString, canBeEmptyIfPost }
+})
+
+export default async function validatePost (ctx, next) {
+  const logger = ctx.logger.ns('validation.post')
+  logger.debug('Validate post')
+
+  const validation = await postValidator.validate(ctx.req.body)
+  if (validation.isValid) {
+    return next()
+  } else {
+    logger.warn('Validate post failed', validation.feedback)
+    logger.debug('Validate post failed', validation)
+
+    ctx.status = 400
+    ctx.body = { feedback: validation.feedback }
+  }
 }
--- api/middleware/validateVote.js
+++ api/middleware/validateVote.js
@@ -1,4 +1,28 @@
-export default function validateVote (ctx, next) {
-  // TODO:
-  return next()
+import { SchemaValidator, isString, isNumber } from '@ianwalter/nrg-validation'
+
+const isVoteValue = {
+  validate (input) {
+    return { isValid: input >= -1 && input <= 1 }
+  }
+}
+const voteValidator = new SchemaValidator({
+  value: { isNumber, isVoteValue },
+  postId: { isString },
+  accountId: { isString }
+})
+
+export default async function validateVote (ctx, next) {
+  const logger = ctx.logger.ns('validation.vote')
+  logger.debug('Validate vote')
+
+  const validation = await voteValidator.validate(ctx.req.body)
+  if (validation.isValid) {
+    return next()
+  } else {
+    logger.warn('Validate vote failed', validation.feedback)
+    logger.debug('Validate vote failed', validation)
+
+    ctx.status = 400
+    ctx.body = { feedback: validation.feedback }
+  }
 }
