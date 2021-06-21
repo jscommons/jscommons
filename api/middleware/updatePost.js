3d1418c9d51862b622c6fbd81711c365799ae50c
Ian Walter <pub@ianwalter.dev>
2021-06-20 20:01:00 -0400

    Updating lint config

--- api/middleware/updatePost.js
+++ api/middleware/updatePost.js
@@ -1,8 +1,6 @@
-export default async function addPost (ctx) {
+export default async function updatePost (ctx) {
   try {
     ctx.logger.debug('Add post', ctx.req.body)
-
-    ctx.body = await prisma.posts.create({ data: ctx.req.body })
   } catch (err) {
     console.error('ERROR', err)
   }
